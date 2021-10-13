e01ae2712c26d6245894210adb7119e596f5554f
none <Osama.Fallatah.med.s@tuwaiq.edu.sa>
2021-10-13 22:45:28 +0300

    cod is fixed

--- Todolist.playground/Contents.swift
+++ Todolist.playground/Contents.swift
@@ -28,8 +28,13 @@ struct List {
     mutating func sortItemByStatus(){
         self.items = items.sorted{$0.taskStatus.hashValue < $1.taskStatus.hashValue}
     }
+    mutating func deleteItem (index:Int ){
+        if items.count > index {
+            items.remove(at: index)}
+        else { print("error handling , you cant remove this item")}
+    }
 }
-   // class for functions use(enum , struct Item ,struct List) and there content
+   // class for functions use(struct List) and its content
 class ToDo {
     var lists: [List] = []
     
@@ -61,22 +66,24 @@ class ToDo {
 
 
 var myLists = ToDo()
-//creat the list
+//  creat the list
 var list_1 = List(listTitle: "Personal_0l")
 //adding the list mylist
 myLists.addNewList(list: list_1)
-//add the items to the list
+// add the items to the list
+
+
 list_1.addNewItem(item: Item(taskTitle: "go to collage", taskStatus: .completed))
 list_1.addNewItem(item: Item(taskTitle: "learn new cods stay till 5pm", taskStatus: .pending))
 list_1.addNewItem(item: Item(taskTitle: "do Tasks in Project", taskStatus:.completed))
 list_1.addNewItem(item: Item(taskTitle: "Grade Projects", taskStatus: .pending))
-list_1.sortItemByStatus()
+//list_1.deleteItem(index: 3)
+//list_1.sortItemByStatus()
 list_1.printAllItems()
-list_1.updateItem(selectindx: 2, newItemTitle: "finish project", isCompelete: .pending)
+//list_1.updateItem(selectindx: 2, newItemTitle: "finish project", isCompelete: .pending)
 var list_2 = List(listTitle: "Gym list")
-myLists.addNewList(list: list_2)
-myLists.updateListTitle(selcectListNumber: 1, newTitleForList: "work list")
-myLists.printAllList()
-
+//myLists.addNewList(list: list_2)
+//myLists.updateListTitle(selcectListNumber: 1, newTitleForList: "work list")
+//myLists.printAllList()
 
 
